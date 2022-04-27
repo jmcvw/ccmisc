6b193f43b5c54de2b41e534d1e6f43f33f032924
jmcvw <43386915+jmcvw@users.noreply.github.com>
2022-04-27 21:33:17 +0100

    Adds ability parameter to 'find_notes()` for dropping duplicated dataset names

--- R/open-notes-files.R
+++ R/open-notes-files.R
@@ -8,12 +8,13 @@
 #' @param day  Integer
 #' @param file_types Character; Any or all of 'notes', 'rmd', or 'data'
 #' @param path Character; Location of course repo
+#' @param drop_dups Logical; Remove duplicated dataset names
 #'
 #' @return List of character vector filepaths named 'notes', 'rmd' and 'data'
 #' @export
 #'
 find_notes <- function(week, day, file_types = c('notes', 'data', 'rmd'),
-                       path) {
+                       path, drop_dups = TRUE) {
 
   path <- sprintf(file.path(path, 'week_%02i/day_%i'), week, day)
 
@@ -31,7 +32,13 @@ find_notes <- function(week, day, file_types = c('notes', 'data', 'rmd'),
 
   if ('data' %in% file_types) {
     data_files <- dir(path, '\\.csv$', recursive = TRUE, full.names = TRUE)
-    data_files_names <- sub('.+/(.+)\\....$', '\\1', data_files)
+    data_files_names <- basename(data_files)
+    if (drop_dups) {
+      dups <- duplicated(data_files_names)
+      data_files <- data_files[!dups]
+      data_files_names <- data_files_names[!dups]
+    }
+    data_files_names <- sub('\\..{3,4}$', '', data_files_names)
     data_files_names <- gsub('[ -]+', '_', data_files_names)
     files$data <- setNames(data_files, data_files_names)
   }
@@ -45,24 +52,22 @@ find_notes <- function(week, day, file_types = c('notes', 'data', 'rmd'),
 #' more datasets have the same name basename, only the last read will be
 #' available
 #'
-#' @param week Integer
-#' @param day Integer
-#' @param file_types Character; Any or all of 'notes', 'rmd', or 'data'
-#' @param path Character; Location of course repo
+#' @param clean_names Logical; Pass dataset columns names through \code{\link{janitor::clean_names}}
+#' @inheritParams find_notes
 #'
-#' @return NULL
+#' @return Character; names of datasets attached.
 #' @export
 #'
 open_notes <- function(week, day, file_types = c('notes', 'data', 'rmd'),
                        path = '/Users/user/Documents/GitHub/data_rewrite2',
-                       clean_names = TRUE) {
+                       clean_names = TRUE, drop_dups = TRUE) {
 
   file_types_arg <- match.arg(file_types, several.ok = TRUE)
   if (missing(file_types)) {
     file_types_arg <- file_types_arg[1:2]
   }
 
-  notes <- find_notes(week, day, file_types_arg, path)
+  notes <- find_notes(week, day, file_types_arg, path, drop_dups)
 
   suppressMessages(
     invisible(
